39b7a25caf51105916d29b5bb092d1754e6e3f97
pfarinac <p.farinac@udc.es>
2024-10-13 20:29:54 +0200

    Documentación de ventana.py

--- src/ventana.py
+++ src/ventana.py
@@ -6,7 +6,7 @@ class Ventana(QWidget):
     def __init__(self):
         super().__init__()
         self.inicializarUI()
-
+    #Añadimos título, medidas y posición a la ventana
     def inicializarUI(self):
         self.setGeometry(100,100,500,500)
         self.setWindowTitle("Ventana")
@@ -14,4 +14,4 @@ class Ventana(QWidget):
 if __name__=="__main__":
     app = QApplication(sys.argv)
     ventana=Ventana()
-    sys.exit(app.exec())
+    sys.exit(app.exec()) # Al cerrar la aplicación se cierra la ventana
